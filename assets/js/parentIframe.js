9d866bd79e249da4cc643d4d05d4db5cf4c7bd27
aporve <aporvep@gmail.com>
2023-04-27 14:36:07 +0530

    comment some code

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -855,13 +855,14 @@ window.addEventListener('message', function (eventData) {
 
     if (parsedData?.event_code == 'profile-details') {
         console.log("\n\n\n <---  profile-details event in parent iframe ---> \n\n\n", parsedData);
-        window.frames.ymIframe.chat.send({
-            event: {
-                code: "profile-details",
-                data: parsedData
-            }
-        }, true);
-        return;
+        console.log('Not sending any event to bot--')
+        // window.frames.ymIframe.chat.send({
+        //     event: {
+        //         code: "profile-details",
+        //         data: parsedData
+        //     }
+        // }, true);
+        // return;
     }
 
     if (parsedData?.event_code == 'associated-accounts') {
