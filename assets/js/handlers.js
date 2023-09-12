91ae2d16beb3571fdc9204f12105dddeb6f6dc94
AporveBhatnagar <aporve@discreteinventions.com>
2023-09-12 18:07:20 +0530

    Fix back condition not working

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -178,6 +178,18 @@ function showSkuLevelDetailsBrand(data, currentSku, requestType, requestSku) {
             if (window['counter_input_wrapper']) {
                 delete window['counter_input_wrapper'];
             }
+            let trimData = window.wholesalerAccountData && window.wholesalerAccountData.filter((whData, index) => {
+                let sum = 0;
+                whData["product_details"].map((detail, ind) => {
+                    sum = sum + Number(detail["units"]);
+                })
+                if(sum != 0) {
+                    return whData;
+                } else {
+                    orderCartData.pop();
+                }
+            });
+            window.wholesalerAccountData = trimData;
             window.dataStore["new_orders"]["orders"] = window.wholesalerAccountData;
         } else {
             console.log("wind --> ", window.wholesalerAccountData);
@@ -277,7 +289,20 @@ function showSkuLevelDetailsBrand(data, currentSku, requestType, requestSku) {
                     isDateSelectedforNewOrder = "";
                 }
             })
-            if (isDateSelectedforNewOrder) {
+            if(isDateSelectedforNewOrder) {
+                let trimData = window.wholesalerAccountData && window.wholesalerAccountData.filter((whData, index) => {
+                    let sum = 0;
+                    whData["product_details"].map((detail, ind) => {
+                        sum = sum + Number(detail["units"]);
+                    })
+                    if(sum != 0) {
+                        return whData;
+                    } else {
+                        orderCartData.pop();
+                    }
+                });
+                window.wholesalerAccountData = trimData;
+                window.dataStore['new_orders']['orders'] = trimData;
                 loadBrandSelectionUI(window.dataStore);
                 let nwData = {
                     ...window.dataStore,
@@ -639,6 +664,7 @@ function showBrandLevelDetails(data, currentSku, requestType, requestSku) {
         e.stopImmediatePropagation();
         let parseData = getParsedData();
         let getSkuAcc = [];
+        let sum = 0;
         console.log("window.counter_input_wrapper -> ", window.counter_input_wrapper)
         getSkuAcc = window.wholesalerAccountData && window.wholesalerAccountData.map((whData, index) => {
             return whData["sku"];
@@ -673,6 +699,18 @@ function showBrandLevelDetails(data, currentSku, requestType, requestSku) {
             if (window['counter_input_wrapper']) {
                 delete window['counter_input_wrapper'];
             }
+            let trimData = window.wholesalerAccountData && window.wholesalerAccountData.filter((whData, index) => {
+                let sum = 0;
+                whData["product_details"].map((detail, ind) => {
+                    sum = sum + Number(detail["units"]);
+                })
+                if(sum != 0) {
+                    return whData;
+                } else {
+                    orderCartData.pop();
+                }
+            });
+            window.wholesalerAccountData = trimData;
             window.dataStore["new_orders"]["orders"] = window.wholesalerAccountData;
         } else {
             console.log("wind --> ", window.wholesalerAccountData);
@@ -754,7 +792,20 @@ function showBrandLevelDetails(data, currentSku, requestType, requestSku) {
                     isDateSelectedforNewOrder = "";
                 }
             })
-            if (isDateSelectedforNewOrder) {
+            if(isDateSelectedforNewOrder) {
+                let trimData = window.wholesalerAccountData && window.wholesalerAccountData.filter((whData, index) => {
+                    let sum = 0;
+                    whData["product_details"].map((detail, ind) => {
+                        sum = sum + Number(detail["units"]);
+                    })
+                    if(sum != 0) {
+                        return whData;
+                    } else {
+                        orderCartData.pop();
+                    }
+                });
+                window.wholesalerAccountData = trimData;
+                window.dataStore['new_orders']['orders'] = trimData;
                 loadBrandSelectionUI(parseData);
                 let nwData = {
                     ...parseData,
